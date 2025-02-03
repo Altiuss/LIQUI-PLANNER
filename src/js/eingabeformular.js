11d84fffef460825683e2dace820e87d02478ba3
Altiuss <altiuss@inbox.lv>
2025-02-03 19:57:28 +0100

    Die fehlerbox

--- src/js/eingabeformular.js
+++ src/js/eingabeformular.js
@@ -61,14 +61,49 @@ const eingabeformular = {
 
             if (formular_fehler.length === 0) {
                 haushaltsbuch.eintrag_hinzufuegen(formulardaten);
+                this.fehlerbox_entfernen();
                 e.target.reset();
                 this.datum_aktualisieren();
             } else {
-
+                this.fehlerbox_entfernen();
+                this.fehlerbox_anzeigen(formular_fehler);
             }
 
         });
     },
+
+    html_fehlerbox_generieren(formular_fehler) {
+        let fehlerbox = document.createElement("div");
+        fehlerbox.setAttribute("class", "fehlerbox");
+
+        let fehlerText = document.createElement("span");
+        fehlerText.textContent = "Es gibt Fehler in folgenden Eingabefelddern:";
+        fehlerbox.insertAdjacentElement("afterbegin", fehlerText);
+
+        let fehlerListe = document.createElement("ul");
+        formular_fehler.forEach(fehler => {
+            let fehlerlistepunkt = document.createElement("li");
+            fehlerlistepunkt.textContent = fehler;
+            fehlerListe.insertAdjacentElement("beforeend", fehlerlistepunkt);
+        });
+        fehlerbox.insertAdjacentElement("beforeend", fehlerListe);
+
+        return fehlerbox;
+    },
+
+    fehlerbox_anzeigen(formular_fehler) {
+        let eingabeformular_container = document.querySelector("#eingabeformular-container");
+        if (eingabeformular_container !== null) {
+            eingabeformular_container.insertAdjacentElement("afterbegin", this.html_fehlerbox_generieren(formular_fehler));
+        }
+    },
+
+    fehlerbox_entfernen() {
+        let bestehende_fehlerbox = document.querySelector(".fehlerbox");
+        if (bestehende_fehlerbox !== null) {
+           bestehende_fehlerbox.remove();
+        }
+},
 
     html_generieren() {
         let eingabeformular = document.createElement("section");
@@ -81,7 +116,7 @@ const eingabeformular = {
             <div class="titel-typ-eingabe-gruppe">
                 <label for="titel">Titel</label>
                 <input type="text" id="titel" form="eingabeformular" name="titel" placeholder="z.B. Einkaufen" size="10"
-                    title="Titel des Eintrags" required>
+                    title="Titel des Eintrags" >
                 <input type="radio" id="einnahme" name="typ" value="einnahme" form="eingabeformular"
                     title="Typ des Eintrags">
                 <label for="einnahme" title="Typ des Eintrags">Einnahme</label>
@@ -94,10 +129,10 @@ const eingabeformular = {
             <div class="betrag-datum-eingabe-gruppe">
                 <label for="betrag">Betrag</label>
                 <input type="number" id="betrag" name="betrag" form="eingabeformular" placeholder="z.B. 10,42" size="10"
-                    step="0.01" title="Betrag des Eintrags (max. zwei Nachkommastellen, kein €-Zeichen)" required>
+                    step="0.01" title="Betrag des Eintrags (max. zwei Nachkommastellen, kein €-Zeichen)">
                 <label for="datum">Datum</label>
                 <input type="date" id="datum" name="datum" form="eingabeformular" placeholder="jjjj-mm-tt" size="10"
-                    title="Datum des Eintrags (Format: jjjj-mm-tt)" required>
+                    title="Datum des Eintrags (Format: jjjj-mm-tt)" >
             </div>
         </div>
         <!-- Absenden-Button -->
