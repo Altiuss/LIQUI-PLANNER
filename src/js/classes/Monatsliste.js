e0f198f419f2116cd2f24836d51ab6a6b5b915db
Altiuss <altiuss@inbox.lv>
2025-02-13 11:10:17 +0100

    Monatslistesammlung added

--- src/js/classes/Monatsliste.js
+++ src/js/classes/Monatsliste.js
@@ -11,7 +11,22 @@
 // </ul>
 // </article>
 
- 
+
 class Monatsliste {
+
+    constructor(jahr, monat) {
+
+        this._jahr = jahr;
+        this._monat = monat;
+        this._eintraege = [];
+        this._bilanz = 0;
+        this._html = this._html_generieren();
+
+    }
+
+    _html_generieren() {
+        
+     }
+
 
 };
