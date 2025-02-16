cb950ea6fa958113621a35378612219242aa43dc
Altiuss <altiuss@inbox.lv>
2025-02-16 11:32:40 +0100

    Monatslistensammlung Teil VIII added

--- src/js/classes/Monatslistensammlung.js
+++ src/js/classes/Monatslistensammlung.js
@@ -8,7 +8,7 @@ class Monatslistensammlung {
         this._html = this._html_generieren();
     }
 
-    eintrag_hinzufuegen(eintrag) {
+    _eintrag_hinzufuegen(eintrag) {
 
         let eintragsmonat = eintrag.datum().toLocaleString("de-DE", { month: "numeric" });
         let eintragsjahr = eintrag.datum().toLocaleString("de-DE", { year: "numeric" });
@@ -22,7 +22,6 @@ class Monatslistensammlung {
         if (!monatsliste_vorhanden) {
             this._monatsliste_hinzufuegen(eintragsjahr, eintragsmonat, eintrag);
         }
-        this._aktualisieren();
     }
 
     _monatsliste_hinzufuegen(jahr, monat, eintrag) {
@@ -63,7 +62,9 @@ class Monatslistensammlung {
 
     }
 
-    _aktualisieren() {
+    aktualisieren(eintraege) {
+        this._monatslisten = [];
+        eintraege.forEach(eintrag => this._eintrag_hinzufuegen(eintrag))
         this._monatslisten_sortieren();
         this._html = this._html_generieren();
         this.anzeigen();
